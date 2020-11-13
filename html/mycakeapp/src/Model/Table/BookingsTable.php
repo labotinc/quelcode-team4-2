51e92326eb10e8d7943219db30a6e5a843c80cce
織田麻子 <12odadao14@gmail.com>
2020-11-13 11:11:11 +0900

    add bake all後のファイル

---
+++ html/mycakeapp/src/Model/Table/BookingsTable.php
@@ -0,0 +1,93 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Bookings Model
+ *
+ * @property \App\Model\Table\UsersTable&\Cake\ORM\Association\BelongsTo $Users
+ * @property \App\Model\Table\SchedulesTable&\Cake\ORM\Association\BelongsTo $Schedules
+ *
+ * @method \App\Model\Entity\Booking get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Booking newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Booking[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Booking|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Booking saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Booking patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Booking[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Booking findOrCreate($search, callable $callback = null, $options = [])
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class BookingsTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('bookings');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsTo('Users', [
+            'foreignKey' => 'user_id',
+            'joinType' => 'INNER',
+        ]);
+        $this->belongsTo('Schedules', [
+            'foreignKey' => 'schedule_id',
+            'joinType' => 'INNER',
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', null, 'create');
+
+        $validator
+            ->scalar('seat_number')
+            ->maxLength('seat_number', 2)
+            ->requirePresence('seat_number', 'create')
+            ->notEmptyString('seat_number');
+
+        $validator
+            ->boolean('is_cancelled')
+            ->notEmptyString('is_cancelled');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['user_id'], 'Users'));
+        $rules->add($rules->existsIn(['schedule_id'], 'Schedules'));
+
+        return $rules;
+    }
+}
