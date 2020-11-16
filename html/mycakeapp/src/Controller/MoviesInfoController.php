8effc9b08f67bb93548654bf4dd27cd64cda95ce
MunehiroMachida <Life1231@outlook.jp>
2020-11-16 02:56:31 +0000

    update_Movieinfo
    コントローラに日付を取得するようにしました

--- html/mycakeapp/src/Controller/MoviesInfoController.php
+++ html/mycakeapp/src/Controller/MoviesInfoController.php
@@ -13,7 +13,15 @@ class MoviesInfoController extends AppController
 
     {
         $this->layout = 'main';
-        // echo "<html><body><h1>Hello!</h1>";
-        // echo "<p>This is sample page.</p></body></html>";
+        $week = array("日", "月", "火", "水", "木", "金", "土", "日", "月", "火", "水", "木", "金", "土");
+        $countWeek = count($week);
+        $today = date("Y-m-d H:i:s");
+
+        for ($i = 0; $i < $countWeek; $i++) {
+            $timestamp = strtotime((string)$i . 'day ' . $today);
+            $weekNumber = ((int)$week[date("w")]) + $i;
+            $weekDate[] = date("m月d日" . "(" . $week[$weekNumber] . ")", $timestamp);
+        }
+        $this->set(compact('weekDate'));
     }
 }
