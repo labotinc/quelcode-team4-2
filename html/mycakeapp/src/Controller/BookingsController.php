b83387c5ee4607111937436b97ecc544f9f317d2
Yuta KTD <61367978+Yuta-KTD@users.noreply.github.com>
2020-11-16 11:50:06 +0900

    [update]ユーザー操作部分としてadd_seatを追加、不正なURL入力を弾く操作も追加

--- html/mycakeapp/src/Controller/BookingsController.php
+++ html/mycakeapp/src/Controller/BookingsController.php
@@ -1,7 +1,11 @@
 <?php
+
 namespace App\Controller;
 
 use App\Controller\AppController;
+
+use Cake\Event\Event;
+use Exception;
 
 /**
  * Bookings Controller
@@ -12,6 +16,18 @@ use App\Controller\AppController;
  */
 class BookingsController extends AppController
 {
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadModel('Users');
+        // ログインユーザー情報を取り出す→本来はログインユーザーを取り出す
+        // 今回は仮ユーザーとして各自で登録するユーザーIDが1の情報を使用
+        // なので、今回レビューを行う際はまずユーザーを一人登録してください。
+        $login_user_info = $this->set('authuser', $this->Users->get(1));
+        // ※認証認可コントローラー完成次第下記に移行。
+        // $login_user_info = $this->set('authuser', $this->Auth->user());
+    }
     /**
      * Index method
      *
@@ -48,8 +64,39 @@ class BookingsController extends AppController
      *
      * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
      */
+    // 管理者画面
     public function add()
     {
+
+        $booking = $this->Bookings->newEntity();
+        if ($this->request->is('post')) {
+            $booking = $this->Bookings->patchEntity($booking, $this->request->getData());
+            if ($this->Bookings->save($booking)) {
+                $this->Flash->success(__('The booking has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The booking could not be saved. Please, try again.'));
+        }
+        $users = $this->Bookings->Users->find('list', ['limit' => 200]);
+        $schedules = $this->Bookings->MovieSchedules->find('list', ['limit' => 200]);
+        $this->set(compact('booking', 'users', 'schedules'));
+    }
+
+    // ユーザー操作画面
+    public function add_seat($schedules_id)
+    {
+        // MovieSchedulesに存在しないIDのURLを直接入力されたときの処理
+        try {
+            // $schedules_idの$movie_scheduleを取得する
+            $movie_schedule = $this->MovieSchedules->get($schedules_id);
+        } catch (Exception $e) {
+            $this->Flash->set(__('不正なURLのため、リダイレクトしました。上映スケジュールページからアクセスしてください。'));
+            // ※トップページが作成されたらトップページをリダイレクト先にする。
+            return $this->redirect(['controller' => 'movie_info', 'action' => 'index']);
+        }
+        $this->viewBuilder()->setLayout('main');
+
         $booking = $this->Bookings->newEntity();
         if ($this->request->is('post')) {
             $booking = $this->Bookings->patchEntity($booking, $this->request->getData());
