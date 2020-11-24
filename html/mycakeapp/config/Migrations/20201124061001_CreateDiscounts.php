e77b1c89059c2d1ab84a3145b4902d505c0d974e
Ryu-y-10 <ryunosuke.yoshimoto1016@gmail.com>
2020-11-24 16:08:35 +0900

    add:マイグレーションファイル作成

---
+++ html/mycakeapp/config/Migrations/20201124061001_CreateDiscounts.php
@@ -0,0 +1,43 @@
+<?php
+
+use Migrations\AbstractMigration;
+
+class CreateDiscounts extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('discounts');
+
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 100,
+            'null' => false
+        ]);
+        $table->addColumn('price', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false
+        ]);
+        $table->addColumn('is_applied', 'boolean', [
+            'default' => 0,
+            'null' => false
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+
+        $table->create();
+    }
+}
