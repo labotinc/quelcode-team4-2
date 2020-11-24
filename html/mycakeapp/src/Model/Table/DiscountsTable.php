6b6929b74f13395dbcced7e230c00bda0994f914
Ryu-y-10 <ryunosuke.yoshimoto1016@gmail.com>
2020-11-24 16:09:48 +0900

    bake allでmvc作成、CRUD作成

---
+++ html/mycakeapp/src/Model/Table/DiscountsTable.php
@@ -0,0 +1,73 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Discounts Model
+ *
+ * @method \App\Model\Entity\Discount get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Discount newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Discount[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Discount|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Discount saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Discount patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Discount[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Discount findOrCreate($search, callable $callback = null, $options = [])
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class DiscountsTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('discounts');
+        $this->setDisplayField('name');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsTo('payment_histories');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', null, 'create');
+
+        $validator
+            ->scalar('name')
+            ->maxLength('name', 100)
+            ->requirePresence('name', 'create')
+            ->notEmptyString('name');
+
+        $validator
+            ->integer('price')
+            ->requirePresence('price', 'create')
+            ->notEmptyString('price');
+
+        $validator
+            ->boolean('is_applied')
+            ->notEmptyString('is_applied');
+
+        return $validator;
+    }
+}
