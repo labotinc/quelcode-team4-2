d2aa7d572f03caf5b5b1e32cd0bdae2e1de86414
織田麻子 <12odadao14@gmail.com>
2020-11-26 11:32:16 +0900

    update クレジットカードに関するバリデーション追加

--- html/mycakeapp/src/Model/Table/CreditCardsTable.php
+++ html/mycakeapp/src/Model/Table/CreditCardsTable.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Model\Table;
 
 use Cake\ORM\Query;
@@ -60,25 +61,77 @@ class CreditCardsTable extends Table
 
         $validator
             ->scalar('card_number')
-            ->maxLength('card_number', 100)
             ->requirePresence('card_number', 'create')
-            ->notEmptyString('card_number');
+            ->notEmptyString('card_number', '空白になっています。')
+            ->naturalNumber('card_number', '半角数字以外の文字が使われています。')
+            ->add('holder_name', 'custom', ['rule' => function ($value, $context) {
+                if (preg_match("/\A[0-9]{16}\z/", $value)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }, 'message' => '不正なカード番号です。']);
 
         $validator
             ->scalar('holder_name')
             ->maxLength('holder_name', 100)
             ->requirePresence('holder_name', 'create')
-            ->notEmptyString('holder_name');
+            ->notEmptyString('holder_name', '空白になっています。')
+            ->add('holder_name', 'custom', ['rule' => function ($value, $context) {
+                if (preg_match("/\A[a-zA-Z]+\s[a-zA-Z]+\z/", $value)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }, 'message' => '半角英字以外の文字が使われています。']);
 
         $validator
             ->scalar('expiration_date')
             ->maxLength('expiration_date', 100)
             ->requirePresence('expiration_date', 'create')
-            ->notEmptyString('expiration_date');
+            ->notEmptyString('expiration_date', '空白になっています。')
+            ->add('expiration_date', 'custom', ['rule' => function ($value, $context) {
+                // 参考ページ http://php.o0o0.jp/article/php-creditcard
+                // 有効期限（MMYY）
+                if (!preg_match('/\A([0-9]{2})([0-9]{2})\z/', $value, $matches)) {
+                    return false; 
+                } else {
+                    $month = $matches[1];
+                    $year = sprintf('20%s', $matches[2]);
+
+                    // 日付妥当性
+                    if (!checkdate($month, 1, $year)) {
+                        return false;
+                    }
+                    // 範囲
+                    $expiration = mktime(0, 0, 0, $month, 1, $year);
+                    // 過去の場合
+                    $today = mktime(0, 0, 0, date('m'), 1, date('Y'));
+                    if ($expiration < $today) {
+                        return false;
+                    }
+                    // 未来の場合（有効期限は最長10年程度？）
+                    $future = mktime(0, 0, 0, date('m'), 1, date('Y') + 10);
+                    if ($expiration > $future) {
+                        return false;
+                    }
+                    return true;
+                }
+            }, 'message' => '有効期限が間違っています。']);
 
         $validator
             ->boolean('is_deleted')
             ->notEmptyString('is_deleted');
+
+        $validator
+            ->notEmptyString('security_code')
+            ->add('security_code', 'custom', ['rule' => function ($value, $context) {
+                if (preg_match("/\A[0-9]{3,4}\z/", $value)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }, 'message' => '半角数字以外の文字が使われているか、あるいは文字数が誤っています。']);
 
         return $validator;
     }
