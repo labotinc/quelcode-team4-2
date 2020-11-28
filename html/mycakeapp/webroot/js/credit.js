0dcb2a09a36e9911c1a3484f244366ba6cc619a4
織田麻子 <12odadao14@gmail.com>
2020-11-28 14:54:30 +0900

    add モーダルウィンドウ

---
+++ html/mycakeapp/webroot/js/credit.js
@@ -0,0 +1,21 @@
+$(function(){
+    //$('main, footer').wrapAll('<div class="modal js-modal">');
+    $('footer').after(add_contents);
+
+    $('#delete').on('click',function(){
+        $('.js-modal').fadeIn();
+        return false;
+    });
+    $('.js-modal-close').on('click',function(){
+        $('.js-modal').fadeOut();
+        return false;
+    });
+});
+
+let add_contents = '<div class="modal js-modal">';
+    add_contents += '<div class="modal__bg js-modal-close"></div>';
+    add_contents += '<div class="modal__content">';
+    add_contents += '<p>本当にこの決済情報を削除しますか？</p>';
+    add_contents += '<a class="js-modal-close" href="">戻る</a>';
+    add_contents += '<button type="submit" class="link-button_small">削除</button>';
+    add_contents += '</div></div>';
