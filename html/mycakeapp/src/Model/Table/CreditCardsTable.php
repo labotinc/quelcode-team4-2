6b89ca8bf32a89f6131c9b61f4ee1e910b81a00f
織田麻子 <12odadao14@gmail.com>
2020-11-29 16:10:44 +0900

    fix where句に条件追加
    削除されたカード情報も取得するようになっていたため

--- html/mycakeapp/src/Model/Table/CreditCardsTable.php
+++ html/mycakeapp/src/Model/Table/CreditCardsTable.php
@@ -158,14 +158,11 @@ class CreditCardsTable extends Table
     }
 
     /**
-     * Returns a rules checker object that will be used for validating
-     * application integrity.
-     *
      * @param string user_id
      * @return array そのuser_idで登録されたクレジットカード情報の一覧（復号化した後、カード番号に関しては下4桁のみ表示）
      */
     public function findCreditCard(string $user_id) {
-        $creditcards = $this->find()->select(['card_number', 'holder_name'])->where(['user_id' => $user_id])->toList();
+        $creditcards = $this->find()->select(['id', 'card_number', 'holder_name'])->where(['user_id' => $user_id, 'is_deleted' => 0])->toList();
         foreach ($creditcards as $creditcard) {
             $creditcard->decrypt();
             $creditcard->card_number = '******' . substr($creditcard->card_number, -4);
