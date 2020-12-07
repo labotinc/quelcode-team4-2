ddb308737176af7c26ec2086f5a91510a3149f76
Yuta KTD <61367978+Yuta-KTD@users.noreply.github.com>
2020-12-07 10:03:38 +0900

    [update]不要なフラッシュメッセージを削除

--- html/mycakeapp/src/Controller/UsersController.php
+++ html/mycakeapp/src/Controller/UsersController.php
@@ -164,11 +164,8 @@ class UsersController extends AppController
         if ($this->request->is('post')) {
             $user = $this->Users->patchEntity($user, $this->request->getData());
             if ($this->Users->save($user)) {
-                $this->Flash->success(__('The user has been saved.'));
-
                 return $this->redirect(['action' => 'thanks']);
             }
-            $this->Flash->error(__('The user could not be saved. Please, try again.'));
         }
         $this->set(compact('user'));
     }
