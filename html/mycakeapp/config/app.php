e4321c7541c4cdc935aa7fa0a33c1e1d25b5bb45
Yuta KTD <61367978+Yuta-KTD@users.noreply.github.com>
2021-01-04 10:48:18 +0900

    [update]config/app.phpの環境変数の受け取り方を修正

--- html/mycakeapp/config/app.php
+++ html/mycakeapp/config/app.php
@@ -265,16 +265,16 @@ return [
             'className' => Connection::class,
             'driver' => Mysql::class,
             'persistent' => false,
-            'host' => getenv(RDS_HOSTNAME),
+            'host' => getenv('RDS_HOSTNAME'),
             /*
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => getenv(RDS_USERNAME),
-            'password' => getenv(RDS_PASSWORD),
-            'database' => getenv(RDS_DB_NAME),
+            'username' => getenv('RDS_USERNAME'),
+            'password' => getenv('RDS_PASSWORD'),
+            'database' => getenv('RDS_DB_NAME'),
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
